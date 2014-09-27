a0b6e3e798dd24fef26c45cfcb9efcb9c246a56a
rachanta <ravikumar.achanta@gmail.com>
2014-09-27 15:25:47 +0530

    POWER: Add an API call to log wake wakeup reason
    Change-Id: I8bf670b07dcd52e29214ebb4e6e42dddd8c3d35e
    Signed-off-by: Ruchi kandoi <kandoiruchi@google.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	arch/arm/common/gic.c

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -265,7 +265,11 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	for (i = find_first_bit(pending, gic->max_irq);
 	     i < gic->max_irq;
 	     i = find_next_bit(pending, gic->max_irq, i+1)) {
-		 log_wakeup_reason(i + gic->irq_offset);
+
+		pr_warning("%s: %d triggered", __func__,
+					i + gic->irq_offset);
+
+		log_wakeup_reason(i + gic->irq_offset);
 	}
 }
 
