83affb61da9c65ef8e76101f842f21768249d5fd
Laxminath Kasam <lkasam@codeaurora.org>
2015-02-10 22:05:35 +0530

    ARM: dt: msm8226: Add AV timer specific data
    ARM: dt: msm8226: Add AV timer specific data
    
    LPASS exposes registers from which timestamp can be
    read by clients for audio & video synchronization.
    Change adds the register address of AV timer in LPASS.
    
    CRs-fixed: 677082
    Change-Id: I4677a745a48913d53d91e7bdeb62ad3ee5358cb3
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>
    Signed-off-by: Shreyas Nagasandra Chandrasekhar <snagas@codeaurora.org>
    
    arch/arm/include/asm/arch_timer.h: fix build - add definitions of arch_timer_get_cntkctl and arch_timer_set_cntkctl

--- arch/arm/include/asm/arch_timer.h
+++ arch/arm/include/asm/arch_timer.h
@@ -10,10 +10,26 @@ struct arch_timer {
 };
 
 #ifdef CONFIG_ARM_ARCH_TIMER
+
 int arch_timer_register(struct arch_timer *);
 int arch_timer_of_register(void);
 cycle_t arch_counter_get_cntpct(void);
+
+static inline u32 arch_timer_get_cntkctl(void)
+{
+	u32 cntkctl;
+	asm volatile("mrc p15, 0, %0, c14, c1, 0" : "=r" (cntkctl));
+	return cntkctl;
+}
+
+static inline void arch_timer_set_cntkctl(u32 cntkctl)
+{
+	asm volatile("mcr p15, 0, %0, c14, c1, 0" : : "r" (cntkctl));
+}
+
+
 #else
+
 static inline int arch_timer_register(struct arch_timer *at)
 {
 	return -ENXIO;
